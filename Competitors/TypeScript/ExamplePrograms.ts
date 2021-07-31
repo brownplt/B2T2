2fbc72a93892325b75a2cf81452554bb6d02763a
Lu, Kuang-Chen <lukuangchen1024@gmail.com>
2021-07-30 20:15:12 -0400

    save

--- Competitors/TypeScript/ExamplePrograms.ts
+++ Competitors/TypeScript/ExamplePrograms.ts
@@ -1,6 +1,6 @@
-import { CTop, parseTable, STop, Table } from './EncodeTables'
-import { buildColumn, dropColumns, getColumn2, getValue, header, nrows, selectColumns1, selectColumns2, selectColumns3, selectRows1, selectRows2, tfilter } from './TableAPI'
-import { filter, fisherTest, map, range, sample, length, startsWith, concat, colNameOfNumber } from './helpers'
+import { CTop, parseRow, parseTable, Row, SchemaOf, STop, Table } from './EncodeTables'
+import { addColumn, addRows, buildColumn, dropColumns, emptyTable, getColumn2, getValue, header, nrows, selectColumns1, selectColumns2, selectColumns3, selectRows1, selectRows2, tfilter } from './TableAPI'
+import { filter, fisherTest, map, range, sample, length, startsWith, concat, colNameOfNumber, average, removeDuplicates } from './helpers'
 import { makeTester } from './unitTest'
 import { gradebook, gradebookMissing, jellyAnon, jellyNamed } from './ExampleTables'
 
@@ -109,21 +109,56 @@ Tester.assertEqual(
 const quizColNames =
     map(
         range(4),
-        (i) => concat("quiz", colNameOfNumber(i))
+        (i) => concat("quiz", colNameOfNumber(i + 1))
     )
 
 const quizTable = selectColumns3(gradebook, quizColNames as ['quiz1', 'quiz2', 'quiz3', 'quiz4'])
 
 const quizAndAverage =
     buildColumn(
-  quizTable,
-  "average",
-  function(r):
-    ns = map(header(r),
-      function(c):
-        getValue(r, c)
-      end)
-    average(ns)
-  end)
+        quizTable,
+        "average",
+        (r) => {
+            const ns = map(header(r),
+                (c) => {
+                    return getValue(r, c)
+                })
+            return average(ns)
+        })
+
+Tester.assertEqual(
+    'quizScoreSelect',
+    () => addColumn(
+        gradebook,
+        "average-quiz",
+        getColumn2(quizAndAverage, "average")),
+    parseTable([
+        ['name', 'age', 'quiz1', 'quiz2', 'midterm', 'quiz3', 'quiz4', 'final', 'average-quiz'],
+        ["Bob", 12, 8, 9, 77, 7, 9, 87, 8.25],
+        ["Alice", 17, 6, 8, 88, 8, 7, 85, 7.25],
+        ["Eve", 13, 7, 9, 84, 8, 8, 77, 8],
+    ]))
+
+
+const tableOfColumn =
+    <C extends CTop, V>(c: C, vs: Array<V>) => {
+        const t1: Table<{}> = addRows(emptyTable, map(vs, (_) => parseRow([])))
+        return addColumn(t1, c, vs)
+    }
+
+const groupByRetentive = <S extends STop, C extends CTop & keyof S>(t: Table<S>, c: C) => {
+  const keys = tableOfColumn("key", removeDuplicates(getColumn2(t, c)))
+  const makeGroup =
+    (kr: Row<SchemaOf<typeof keys>>) => {
+      const k = getValue(kr, "key")
+      return tfilter(t,
+        (r) => {
+          return getValue(r, c) == k
+        })
+      }
+  return buildColumn(keys, "groups", makeGroup)
+}
+
+// TODO test groupByRetentive
 
 Tester.go()
